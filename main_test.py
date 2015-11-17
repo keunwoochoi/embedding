99aa9459686f6c8597d23695f758f1bb4c817950
gnuchoi <gnuchoi@gmail.com>
2015-11-17 00:43:12 +0000

    code draft

--- main_test.py
+++ main_test.py
@@ -2,6 +2,7 @@ import platform
 import os
 import sys
 import cPickle as cP
+import numpy as np
 
 device_name = platform.node()
 
@@ -21,6 +22,7 @@ if isServer:
 
 	PATH_HOME = "/homes/kc306/"
 	PATH_WORK = PATH_HOME + "embedding/"
+	PATH_DATA = PATH_WORK + "data/"
 
 
 # [0] Pre-processing: make my own dictionary.
@@ -33,7 +35,32 @@ with open(PATH_ILM_META + r"collection10000-810(500)-5(1)-loglikgmm-wav.csv", 'r
 		song_id, song_path = line.split('","') #	line:"444","e/audio/x-wav/eb/1508-04.01.wav"
 		dict_id_path[int(song_id)] = song_path.split("/")[-1]
 
-cP.dump(dict_id_path, )
+cP.dump(dict_id_path, PATH_DATA + "id_path_dict.cP" )
+
+moodnames = []
+with open(PATH_ILM_ACT + "moodnames.txt", 'r') as moodnames_fp:
+	for line in moodnames_fp:
+		moodnames.append(line.rstrip("\n"))
+
+cP.dump(moodnames, PATH_DATA + "moodnames.cP")
+
+track_ids = []
+with open(PATH_ILM_ACT + "trackids.txt", 'r') as trackids_fp:
+	for line in trackids_fp:
+		track_ids.append(int(line.rstrip("\n")))
+
+cP.dump(track_ids, PATH_DATA + "track_ids.cP")
+
+tags_matrix = []
+with open(PATH_ILM_ACT + "genretags.txt", 'r') as tags_fp:
+	for line in tags_fp:
+		line_array = line.rstrip("\n").split(" ")
+		tags_matrix.append(line_array)
+
+np_tags_matrix = np.array(tags_matrix)
+np.save(PATH_DATA + "tags_matrix.np", np_tags_matrix)
+
+
 
 
 
