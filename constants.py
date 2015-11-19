9d016b2f60ab5f093cb898ce4a25a2df73e9d899
gnuchoi <gnuchoi@gmail.com>
2015-11-19 18:00:01 +0000

    new files

--- constants.py
+++ constants.py
@@ -4,10 +4,10 @@ N_FFT = 1024
 WIN_LEN = 1024
 HOP_LEN = 512 # 11 sec --> 512 frames
 
-'''
-file_dict = {}
-file_dict["id_path"] = "id_path_dict.cP" #dict
-file_dict["moodnames"] = "moodnames.cP" #list
-file_dict["track_ids"] = "track_ids.cP" #list
-file_dict["mood_tags_matrix"] = "mood_tags_matrix.npy" # matrix, 9320-by-100
-'''
+
+FILE_DICT = {}
+FILE_DICT["id_path"] = "id_path_dict.cP" #dict
+FILE_DICT["moodnames"] = "moodnames.cP" #list
+FILE_DICT["track_ids"] = "track_ids.cP" #list
+FILE_DICT["mood_tags_matrix"] = "mood_tags_matrix.npy" # matrix, 9320-by-100
+
