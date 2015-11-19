144c7711c215de8029a1eb283246027d76f172e3
gnuchoi <gnuchoi@gmail.com>
2015-11-19 18:39:04 +0000

    new files

--- my_keras_models.py
+++ my_keras_models.py
@@ -29,8 +29,8 @@ def build_convnet_model(height, width, num_labels):
 	dropouts = [0] + [0.25]*(num_layers-1)
 
 	for i in xrange(num_layers):
-		print i
 		model.add(Convolution2D(num_stacks[i+1], num_stacks[i], image_patch_sizes[i][0], image_patch_sizes[i][1], border_mode='same', activation='relu'))
+		model.add(MaxPooling2D(poolsize=pool_sizes[i]))
 		if dropouts[i] != 0:
 			model.add(Dropout(dropouts[i]))
 		if i != 0:
