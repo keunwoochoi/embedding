1630f40c2c99074f1a18563c3d01ace778d9496d
gnuchoi <gnuchoi@gmail.com>
2015-11-19 18:06:40 +0000

    new files

--- main_learn_tag.py
+++ main_learn_tag.py
@@ -11,25 +11,25 @@ import my_keras_models
 import cPickle as cP
 
 class File_Manager():
-	def __init__():
+	def __init__(self):
 		self.track_ids = cP.load(open(PATH_DATA + FILE_DICT["track_ids"], 'r')) #list, 9320
 		self.id_path = cP.load(open(PATH_DATA + FILE_DICT["id_path"], 'r')) #dict, 9320
 		self.filenum = len(self.track_ids)
 		print "file manager init with %d track ids and %d element dictionary " % (self.filenum, len(self.id_path))
 
-	def load_src(ind):
+	def load_src(self, ind):
 		if ind > len(self.track_ids):
 			print 'wrong ind -- too large: %d' % ind
 		path = self.id_path[self.track_ids[ind]]
 		return librosa.load(path, sr=SR, mono=False)
 
-	def load_stft(ind):
+	def load_stft(self, ind):
 		return np.load( PATH_STFT + str(sef.track_ids[ind]) + '.npy')
 
-	def load_cqt(ind):
+	def load_cqt(self, ind):
 		return np.load( PATH_CQT + str(sef.track_ids[ind]) + '.npy')
 
-	def split_inds(num_folds):
+	def split_inds(self, num_folds):
 		"""returns index of train/valid/test songs"""
 		if num_folds < 3:
 			return "wrong num_folds, should be >= 3"
