756ed8bbbc5c9a32a4a4dbf2ac33d62cb5408d42
gnuchoi <gnuchoi@gmail.com>
2015-11-21 03:43:33 +0000

    change learn_tag file for latent feature learning

--- my_keras_models.py
+++ my_keras_models.py
@@ -26,7 +26,7 @@ def build_convnet_model(height, width, num_labels):
 	image_patch_sizes = [[3,3]]*num_layers
 	pool_sizes = [(2,2)]*num_layers
 	num_stacks = [48]*num_layers
-	dropouts = [0] + [0.25]*(num_layers-1)
+	dropouts = [0] + [0]*(num_layers-1)
 
 	for i in xrange(num_layers):
 		if i == 0:
@@ -48,7 +48,7 @@ def build_convnet_model(height, width, num_labels):
 	model.add(Dense(256, init='normal', activation='relu'))
 	model.add(Dropout(0.5))
 	model.add(Dense(num_labels, init='normal', activation='softmax'))
-	rmsprop = RMSprop(lr=1e-6, rho=0.9, epsilon=1e-6)
+	rmsprop = RMSprop(lr=5e-5, rho=0.9, epsilon=1e-6)
 	print '--- ready to compile keras model ---'
 	model.compile(loss='mean_squared_error', optimizer=rmsprop)
 	print '--- complie fin. ---'
