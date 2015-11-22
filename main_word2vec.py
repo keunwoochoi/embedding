c1234a24798c9561dd59e44dc9745ab04c83039b
gnuchoi <gnuchoi@gmail.com>
2015-11-22 01:14:45 +0000

    word2vec

---
+++ main_word2vec.py
@@ -0,0 +1,117 @@
+""" Apply word2vec to all tags (100 from ilm10k) and 
+"""
+from constants import *
+from environments import *
+import gensim
+import logging
+import os.path
+import sys
+import multiprocessing
+import cPickle as cP
+
+from gensim.corpora import WikiCorpus
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+# http://williambert.online/2012/05/relatively-quick-and-easy-gensim-example-code/
+# background_corpus = gensim.TextCorpus()
+
+def generate_lines(corpus):
+    for index, text in enumerate(corpus.get_texts()):
+        if index < max_sentence or max_sentence==-1:
+            yield text
+        else:
+            break
+
+def init_logger():
+	program = os.path.basename(sys.argv[0])
+	logger = logging.getLogger(program)
+
+	logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+	logging.root.setLevel(level=logging.INFO)
+	logger.info("running %s" % ' '.join(sys.argv))
+	return logger
+
+def prepare_wiki_text():
+	# [1] prepare wiki text
+	logger = init_logger()
+	inp = 'enwiki-20151102-pages-articles.xml.bz2'
+	outp = 'wiki.en.text'
+	if os.path.exists(PATH_WIKI + outp):
+		print 'prepare_wiki_text : already done. remove %s if you want to run it again.' % outp
+		return
+	output = open(PATH_WIKI + outp, 'w')
+	wiki = WikiCorpus(inp, lemmatize=False, dictionary={})
+	for text in wiki.get_texts():
+		output.write(space.join(text) + "\n")
+		i += 1
+		if (i % 10000 == 0):
+			logger.info("Saved " + str(i) + " articles.")
+
+	output.close()
+	logger.info("Finished saved " + str(i) + " articles.")	
+
+def train_word2vec_model():
+
+	logger = init_logger()
+	inp = 'wiki.en.text'
+	outp = 'wiki.en.text.model'
+	outp2= 'wiki.en.text.vector'
+	if os.path.exists(PATH_WIKI + outp2):
+		print 'train_word2vec_model : already done. remove %s if you want to train it again.' % outp2
+		model = gensim.models.Word2Vec.load_word2vec_format(outp2, binary=False) # takes long.
+		return model
+
+	model = Word2Vec(LineSentence(inp), size=200, window=5, min_count=10, workers=multiprocessing.cpu_count())
+
+	# model.init_sims(replace=True) 
+	model.save(PATH_WIKI + outp)
+	model.save_word2vec_format(PATH_WIKI + outp2, binary=False)
+	print "Fin.; training word2vec model."
+	return model
+
+if __name__ == "__main__":
+
+	'''
+	bow_corpus = gensim.corpora.MmCorpus(PATH_WIKI + "_tfidf.mm", background_corpus)
+	# http://stackoverflow.com/questions/23735576/gensim-train-word2vec-on-wikipedia-preprocessing-and-parameters
+	corpus = gensim.corpora.WikiCorpus(PATH_WIKI + 'enwiki-20151102-pages-articles.xml.bz2', dictionary=False)
+	max_sentence = -1
+
+	# dictionary = gensim.corpora.Dictionary.load_from_text(PATH_WIKI + '_wordids.txt')
+	model = gensim.models.word2vec.Word2Vec()
+	model.build_vocab(generate_lines())
+	model.train(generate_lines(), chunksize=500)
+
+	### https://radimrehurek.com/gensim/wiki.html
+	# load id->word mapping (the dictionary)
+	id2word = gensim.corpora.Dictionary.load_from_text('wiki_en_wordids.txt')
+	# load corpus iterator
+	mm = gensim.corpora.MmCorpus('wiki_en_tfidf.mm')
+	print(mm) # MmCorpus(3931787 documents, 100000 features, 756379027 non-zero entries)
+	'''
+
+	# http://textminingonline.com/training-word2vec-model-on-english-wikipedia-by-gensim
+	# [1]
+	prepare_wiki_text()
+
+	# [2] training a word2vec model. Or load it if it's done before.
+	model = train_word2vec_model()
+
+	# [3] use the model to get each tag's embedding vector
+	moodnames = cP.load(open(PATH_DATA + FILE_DICT["moodnames"], 'r')) #list, 100
+	embeddings = {}
+	for moodname in moodnames:
+		try:
+			embeddings[moodname]=model['moodname']
+		except:
+			pass
+	cP.dump(embeddings, open(PATH_DATA + FILE_DICT["mood_embeddings"], 'w'))
+
+	
+
+
+
+
+
+
+
---
+++ my_keras_utils.py
@@ -0,0 +1,26 @@
+import keras
+
+
+class History(keras.callbacks.Callback):
+	"""history, not validation. use History_Val to include both training and validation data"""
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accs = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accs.append(logs.get('acc'))
+
+class History_Val(keras.callbacks.Callback):
+	"""history with valudation data"""
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accs = []
+        self.val_losses = []
+        self.val_accs = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accs.append(logs.get('acc'))
+    	self.val_losses.append(logs.get('val_loss'))
+    	self.val_accs.append(logs.get('val_acc'))
