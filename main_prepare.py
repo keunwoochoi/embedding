33c3d14e14a0b8df4075860f1c71c77500a0d9f5
gnuchoi <gnuchoi@gmail.com>
2015-11-27 02:21:17 +0000

    modify getLDA to store topic strings

--- main_prepare.py
+++ main_prepare.py
@@ -303,7 +303,7 @@ if __name__=="__main__":
 				np.save(PATH_DATA + filename_out, W)
 	# analysis - LDA 
 	if True and "it is already done.":
-		for k in [2,3,5,10,20]:
+		for k in xrange(2:21):
 			filename_out = FILE_DICT["mood_latent_tfidf_matrix"] % k
 			if os.path.exists(PATH_DATA + filename_out) and False:
 				W = np.load(PATH_DATA + filename_out)
