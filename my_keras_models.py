8082bb6ca236c290c9fd95d1cbd46a2bc05803d7
gnuchoi <gnuchoi@gmail.com>
2015-12-07 19:22:26 +0000

    remove librosa import

--- my_keras_models.py
+++ my_keras_models.py
@@ -68,7 +68,7 @@ def build_regression_convnet_model(height, width, num_labels, num_layers=5, mode
 	for i in xrange(num_layers):
 		if i == 0:
 			model.add(Convolution2D(num_stacks[i], image_patch_sizes[i][0], image_patch_sizes[i][1], border_mode='same', input_shape=(2, height, width), activation='tanh' ))
-		else:ㅂ 
+		else:
 			model.add(Convolution2D(num_stacks[i], image_patch_sizes[i][0], image_patch_sizes[i][1], border_mode='same', activation='tanh'))
 		model.add(MaxPooling2D(pool_size=pool_sizes[i], ignore_border=True))
 
