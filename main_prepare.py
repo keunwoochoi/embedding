a84c0261497616bdbfd365619afcd1cf65a4117c
keunwoochoi <gnuchoi+github@gmail.com>
2015-12-19 17:31:16 +0000

    add print to debug

--- main_prepare.py
+++ main_prepare.py
@@ -266,7 +266,7 @@ def process_harmonigram(track_id):
 
 def process_all_about_cqt(track_id):
 	'''do hps_on_cqt, chroma, pitchgram'''
-	if not os.path.exists(PATH_CQT + str(track_id) + 'npy'):
+	if not os.path.exists(PATH_CQT + str(track_id) + '.npy'):
 		print 'cqt missing: will do: %d' % track_id
 		process_cqt(track_id)
 
