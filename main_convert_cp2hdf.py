50db60cd5af027f0732fc5cb41293e19ba17a2f3
keunwoo choi <gnuchoi+github@gmail.com>
2015-12-26 15:35:02 +0000

    prepare to convert cpickle to hdf

--- main_convert_cp2hdf.py
+++ main_convert_cp2hdf.py
@@ -19,21 +19,21 @@ def create_hdf_dataset(filename, dataset_name, file_manager, song_file_inds):
 	clips_per_song = 3
 	num_songs = len(song_file_inds)
 	num_clips = clips_per_song*num_songs
-	# create hdf file.
+
+	# create or load dataset
 	if os.path.exists(PATH_HDF + filename):
 		file_write = h5py.File(PATH_HDF + filename, 'r')
 		print 'loading hdf file that exists already there.'
 	else:
 		file_write = h5py.File(PATH_HDF + filename, 'w')
+		data_cqt = file_write.create_dataset(dataset_name, (num_clips, 1, tf_height, tf_width), maxshape=(None, None, None, None)) #(num_samples, num_channel, height, width)
 		print 'creating new hdf file.'
-	if dataset_name == 'cqt':
-		tf_representation = file_manager.load_cqt(0) # change to more general name than 'tf_represnetation'
+
+	if dataset_name in ['cqt', 'stft']:
+		tf_representation = file_manager.load(ind=0, data_type=dataset_name) # change to more general name than 'tf_represnetation'
 
 	tf_height, num_fr_temp, num_ch = tf_representation.shape # 513, 6721, 2 for example.
-	tf_width = int(6 * CQT_CONST["frames_per_sec"]) # 6-seconds
-	# create dataset
-	data_cqt = file_write.create_dataset(dataset_name, (num_clips, 1, tf_height, tf_width), maxshape=(None, None, None, None)) #(num_samples, num_channel, height, width)
-	
+	tf_width = int(6 * CQT_CONST["frames_per_sec"]) # 6-seconds		
 	tf_stereo = np.zeros((tf_height, tf_width, 2))
 	# tf_downmix = np.zeros((tf_height, tf_width, 1))
 	# fill the dataset.
