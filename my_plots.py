87cb2f29e93a5d59408387d164776fef1a082769
keunwoochoi <gnuchoi+github@gmail.com>
2015-12-29 16:09:15 +0000

    prepare to learn with hdf

--- my_plots.py
+++ my_plots.py
@@ -130,7 +130,7 @@ def save_weight_as_image(W, save_path, filename_prefix, normalize, mono, layerin
 		W = W[:,ind,:,:]
 		filename = 'weights_' + repr(layerind) + '_' + filename_prefix + '_' + repr(ind) + '.png'
 		mosaic = make_mosaic(imgs=W, normalize=normalize, border=2)
-		mosaic = int(mosaic * 2**8)
+		mosaic = np.array((mosaic * 2**8), dtype=np.int8)
 		write_png(save_path + filename, mosaic)
 
 	else:
