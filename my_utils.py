a7be068c4a07ecd805f71764bdaf56b5cb7010ab
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-01 23:24:37 +0000

    remove keras visualisation, change loading of y as numpy

--- my_utils.py
+++ my_utils.py
@@ -271,7 +271,7 @@ def load_all_labels(n_dim=None, num_fold=10, clips_per_song=3):
 	train_inds, valid_inds, test_inds = file_manager.split_inds(num_folds=5)
 	return load_y(train_inds, n_dim, clips_per_song), \
 			load_y(valid_inds, n_dim, clips_per_song), \
-			load(test_inds, n_dim, clips_per_song)
+			load_y(test_inds, n_dim, clips_per_song)
 
 def load_all_sets_from_hdf(tf_type=None, n_dim=None, task_cla=False):
 	'''using hdf. perhaps you should set PATH_HDF_LOCAL for the machine you're using.
