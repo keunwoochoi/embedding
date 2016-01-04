58b25bc4a50292e13f1bd6f9a4423fedc035c594
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-04 13:23:20 +0000

    results

--- run_tag_learnings.sh
+++ run_tag_learnings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# python main_learn_tag_hdf.py -tf mfcc -l 6
+#python main_learn_tag_hdf.py -tf mfcc -l 3 -fm 64
 
 
 #python main_learn_tag_hdf.py -tf cqt -l 5
 #python main_learn_tag_hdf.py -tf cqt -l 6
-python main_learn_tag_hdf.py -tf stft -l 5
-python main_learn_tag_hdf.py -tf stft -l 6
+python main_learn_tag_hdf.py -tf stft -l 5 -ne 45
+python main_learn_tag_hdf.py -tf stft -l 6 -ne 45
 
