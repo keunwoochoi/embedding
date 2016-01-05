fb63485c4476d0b0637b475efdac6f94b3a5157d
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-05 14:39:12 +0000

    more on test

--- main_learn_tag_hdf.py
+++ main_learn_tag_hdf.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
 										nb_epoch=TR_CONST["num_epoch"], 
 										show_accuracy=False, 
 										verbose=1, 
-										callbacks=[history,  checkpointer],
+										callbacks=[history,  weight_image_saver],
 										shuffle=False)
 		else:
 			model.fit(train_x, train_y, validation_data=(valid_x, valid_y), 
@@ -252,7 +252,7 @@ if __name__ == "__main__":
 										nb_epoch=TR_CONST["num_epoch"], 
 										show_accuracy=False, 
 										verbose=1, 
-										callbacks=[history, early_stopping, weight_image_saver,weight_image_saver],
+										callbacks=[history, weight_image_saver, early_stopping, checkpointer],
 										shuffle=False)
 		loss_testset = model.evaluate(test_x, test_y, show_accuracy=False, batch_size=batch_size)
 	else:
