9b3ca274e081ac2cfe3422337a2310347e34decb
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-07 20:28:18 +0000

    results for lrely, prelu, elu

--- run_tag_learnings.sh
+++ run_tag_learnings.sh
@@ -3,6 +3,15 @@
 
 
 
-python main_learn_tag_hdf.py -l 4 -ne 30
-python main_learn_tag_hdf.py -l 6 -ne 30
+# python main_learn_tag_hdf.py -ne 5 -op adam
+#python main_learn_tag_hdf.py -ne 8 -op sgd
+python main_learn_tag_hdf.py -ne 3 -op adagrad -act lrelu
+python main_learn_tag_hdf.py -ne 3 -op adagrad -act prelu
+python main_learn_tag_hdf.py -ne 3 -op adagrad -act elu
+
+python main_learn_tag_hdf.py -ne 3 -op sgd -act lrelu
+python main_learn_tag_hdf.py -ne 3 -op sgd -act prelu
+python main_learn_tag_hdf.py -ne 3 -op sgd -act elu
+#python main_learn_tag_hdf.py -ne 5 -op adadelta
+#python main_learn_tag_hdf.py -ne 5 -op rmsprop
 
