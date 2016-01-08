88ec08d1a740addf96bccd44d21c34035a077c39
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-08 09:21:33 +0000

    results

--- run_tag_learnings.sh
+++ run_tag_learnings.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #python main_learn_tag_hdf.py -ne 10 -op adagrad -act elu
-python main_learn_tag_hdf.py -ne 30 -op rmsprop -act elu
-python main_learn_tag_hdf.py -ne 30 -op adadelta -act elu
-python main_learn_tag_hdf.py -ne 30 -op adam -act elu
+sleep 4h
+python main_learn_tag_hdf.py -ne 7 -op rmsprop -act elu
+python main_learn_tag_hdf.py -ne 7 -l 4 -op adadelta -act elu
+python main_learn_tag_hdf.py -ne 7 -op adam -act elu
 
-python main_learn_tag_hdf.py -ne 30 -op adagrad -act prelu
-python main_learn_tag_hdf.py -ne 30 -op rmsprop -act prelu
-python main_learn_tag_hdf.py -ne 30 -op adadelta -act prelu
-python main_learn_tag_hdf.py -ne 30 -op adam -act prelu
+python main_learn_tag_hdf.py -ne 7 -l 4 -op adagrad -act prelu
+python main_learn_tag_hdf.py -ne 7 -op rmsprop -act prelu
+python main_learn_tag_hdf.py -ne 7 -l 4 -op adadelta -act prelu
+python main_learn_tag_hdf.py -ne 7 -op adam -act prelu
 
  
-python main_learn_tag_hdf.py -ne 30 -op adagrad -act lrelu
-python main_learn_tag_hdf.py -ne 30 -op rmsprop -act lrelu
-python main_learn_tag_hdf.py -ne 30 -op adadelta -act lrelu
-python main_learn_tag_hdf.py -ne 30 -op adam -act lrelu
+python main_learn_tag_hdf.py -ne 7 -l 4 -op adagrad -act lrelu
+python main_learn_tag_hdf.py -ne 7 -op rmsprop -act lrelu
+python main_learn_tag_hdf.py -ne 7 -l 4 -op adadelta -act lrelu
+python main_learn_tag_hdf.py -ne 7 -op adam -act lrelu
 
