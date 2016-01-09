6d0883a1bea548e6370cdafca34b05a11e42e243
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-09 19:04:44 +0000

    very large value for conv regularise w l1

--- training_settings.py
+++ training_settings.py
@@ -18,7 +18,7 @@ TR_CONST["num_layers"] = 4 # can be overriden
 TR_CONST["num_feat_maps"] = [48]*TR_CONST["num_layers"]
 TR_CONST["activations"] = ['prelu']*TR_CONST["num_layers"]
 TR_CONST["dropouts"] = [0.0]*TR_CONST["num_layers"]
-TR_CONST["regulariser"] = [('l1', 3e-10)]*TR_CONST["num_layers"] # use [None] not to use.
+TR_CONST["regulariser"] = [('l1', 3e-2)]*TR_CONST["num_layers"] # use [None] not to use.
 
 TR_CONST["num_fc_layers"] = 2
 TR_CONST["dropouts_fc_layers"] = [0.0]*TR_CONST["num_fc_layers"]
