e6aae5ea1c021a5e07e51c9fea77d2e30f4cdc2e
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-10 01:57:23 +0000

    more feature maps :72

--- main_learn_tag_hdf.py
+++ main_learn_tag_hdf.py
@@ -266,9 +266,9 @@ if __name__ == "__main__":
 										mono=True)
 
 	if TR_CONST["tf_type"] == 'cqt':
-		batch_size = 32
+		batch_size = 24
 	elif TR_CONST["tf_type"] == 'stft':
-		batch_size = 16
+		batch_size = 12
 	elif TR_CONST["tf_type"] == 'mfcc':
 		batch_size = 48
 	else:
