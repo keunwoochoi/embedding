482411573f2e47f246a5b24bf7b814568423cb5a
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-14 01:03:36 +0000

    sainty check

--- main_prepare.py
+++ main_prepare.py
@@ -270,9 +270,9 @@ def process_melgram(track_id):
 	if os.path.exists(PATH_MELGRAM + str(track_id) + '.npy'):
 		if os.path.getsize(PATH_MELGRAM + str(track_id) + '.npy') != 0:
 			print "melgram: skip this id: %d, it's already there!" % track_id
-	else:
-		src, sr = load_src(track_id)
-		do_melgram(src, track_id)
+	
+	src, sr = load_src(track_id)
+	do_melgram(src, track_id)
 
 def prepare_transforms_detail(num_process, ind_process, task, isTest):
 
