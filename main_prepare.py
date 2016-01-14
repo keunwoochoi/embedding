d81e9946dbfb2399b114a6202614831d1de25386
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-14 01:09:28 +0000

    sainty check

--- main_prepare.py
+++ main_prepare.py
@@ -268,8 +268,8 @@ def process_stft_cqt(track_id):
 
 def process_melgram(track_id):
 	if os.path.exists(PATH_MELGRAM + str(track_id) + '.npy'):
-		if os.path.getsize(PATH_MELGRAM + str(track_id) + '.npy') != 0:
-			print "melgram: skip this id: %d, it's already there!" % track_id
+		# if os.path.getsize(PATH_MELGRAM + str(track_id) + '.npy') != 0: # have no permission
+		print "melgram: skip this id: %d, it's already there!" % track_id
 	
 	src, sr = load_src(track_id)
 	do_melgram(src, track_id)
