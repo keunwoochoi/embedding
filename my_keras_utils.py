b6c20a8905dc3625e0308435ccb2f09ca3463cf9
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-21 17:10:44 +0000

    remove local plot

--- my_keras_utils.py
+++ my_keras_utils.py
@@ -97,10 +97,10 @@ class Weight_Image_Saver(keras.callbacks.Callback):
 
 	def on_epoch_end(self, epoch, logs={}):
 		#seconds = str(int(time.time()))
-		my_plots.save_model_as_image(self.model, save_path=self.path_to_save, 
-												filename_prefix='local_', 
-												normalize='local', 
-												mono=True)
+		# my_plots.save_model_as_image(self.model, save_path=self.path_to_save, 
+		# 										filename_prefix='local_', 
+		# 										normalize='local', 
+		# 										mono=True)
 		my_plots.save_model_as_image(self.model, save_path=self.path_to_save, 
 												filename_prefix='global_', 
 												normalize='global', 
