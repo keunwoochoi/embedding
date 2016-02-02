65eb1f088bdde6b1ad001db53a623cb4119c1634
keunwoochoi <gnuchoi+github@gmail.com>
2016-02-02 19:49:20 +0000

    remove last fc l1 forcing (which was not applied anyway)

--- my_keras_models.py
+++ my_keras_models.py
@@ -299,7 +299,7 @@ def design_2d_convnet_model(setting_dict):
 	for fc_idx in xrange(num_fc_layers):
 		# setup regulariser
 		W_regularizer = get_regulariser(setting_dict['regulariser_fc_layers'][fc_idx])
-		act_regularizer = get_regulariser(setting_dict['act_regulariser_fc_layres'][fc_idx])
+		act_regularizer = get_regulariser(setting_dict['act_regulariser_fc_layers'][fc_idx])
 		
 		if setting_dict['maxout']:
 			# maxout...	
